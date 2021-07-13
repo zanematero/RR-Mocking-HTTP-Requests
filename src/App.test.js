341854e0f5b4389b7f31d199621b5280bb906cda
nkleinmann <nekleinmann@gmail.com>
2021-07-13 09:09:19 -0400

    Cleared app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,3 @@
-import { render, screen } from '@testing-library/react'
-import App from './App'
 
-test('renders github profile text', () => {
-  render(<App />)
-  const textElement = screen.getByText(/Github info/i);
-  expect(textElement).toBeInTheDocument();
-});
+
+
