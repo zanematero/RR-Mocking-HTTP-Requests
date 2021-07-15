486a0b611e5cdbc6269e1fbf8ccfd8c0a34fd6fc
nkleinmann <nekleinmann@gmail.com>
2021-07-15 11:43:14 -0400

    Update starter code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import './App.css'
-import Col from "react-bootstrap/Col"
-import Row from "react-bootstrap/Row"
 
 
 function App() {
   return (
     <div className="App">
-      <Row>
-        <Col>Github Profile Info:</Col>
-      </Row>
+      <h1>Github Profile Info:</h1>
     </div>
   );
 }
