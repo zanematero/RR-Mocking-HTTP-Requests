2554c004da9b59fa900300ffa952285b2f015282
Zane Matero <materozane@gmail.com>
2023-03-22 14:30:41 -0700

    issues with importing.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 
 function App() {
-  return (
+const [gitHubName, setGitHubName] = useState('')                     
+
+
+useEffect(() => {
+    fetch('https://api.github.com/users/zanematero')
+    .then(res => res.json())
+    .then(data => {
+        setGitHubName(data.name)
+    })
+}, [])
+
+return (
     <div className="App">
-      <h1>Github Profile Info:</h1>
+        <h1>Github Profile Info:</h1>
+        <h2>{gitHubName}</h2>
     </div>
-  );
+);
 }
 
-export default App;
+export default App
+    
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,14 @@
+import { render, screen, waitFor } from '@testing-library/react'
+require('jest-fetch-mock').enableMocks()
 
+test("receives GitHub name from GitHub REST API using jest fetch mock", async () => {
+    fetch.mockResponseOnce(JSON.stringify({name: 'zane'}))
+    render(<App />)
+    const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+    expect(gitHubName).toHaveTextContent('zane')
+  })
 
-
+beforeEach(() => {
+    // sets everything back to initial state before each test
+    fetch.resetMocks();
+  })
